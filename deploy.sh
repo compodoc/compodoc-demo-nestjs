c2b39b38649d6df5e82d98186e68c52c16ba20f0
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2019-01-17 10:36:48 +0100

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
+echo "Starting deployment"
+echo "Target: gh-pages branch"
 
-GIT_DEPLOY_REPO=${GIT_DEPLOY_REPO:-$(node -e 'process.stdout.write(require("./package.json").repository)')}
+GIT_DEPLOY_REPO=${GIT_DEPLOY_REPO:-$(node -p -e "require('./package.json').repository.url")}
 
 cd documentation && \
 
 rm -R .git
 git init && \
 
+
+echo "Pushing new content to $ORIGIN_URL"
 git config user.name "Travis CI" && \
 git config user.email "github@travis-ci.org" && \
 
 git add . && \
 git commit -m "Deploy to GitHub Pages" && \
 
-git push --force "${GIT_DEPLOY_REPO}" master:gh-pages
+git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
